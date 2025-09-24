28ad29c95929f0d036fbec90cb55d89746f662e4
Mashudu Molema <mrkaymp@gmail.com>
2025-09-24 09:46:17 +0200

    Updated build.sh

--- build.sh
+++ build.sh
@@ -5,15 +5,6 @@ set -e
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
-echo "Installing Node dependencies for Tailwind..."
-cd tailwind
-npm install
-
-echo "Building Tailwind CSS..."
-npm run build
-
-cd ..
-
 echo "Collecting Django static files..."
 python manage.py collectstatic --noinput
 
