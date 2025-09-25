ab07b4a1aa6078b32acedae4f5a5da39cf6078be
Mashudu Molema <mrkaymp@gmail.com>
2025-09-25 18:06:12 +0200

    Prepared HireSpot-Frontend for deploying on Vercel

--- job_board_frontend_project/utils/api.ts
+++ job_board_frontend_project/utils/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api", 
-  withCredentials: false,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 export default api;
