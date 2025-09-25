3f48b420d67cee36cb1969086137bb6ff3c2f2d7
Mashudu Molema <mrkaymp@gmail.com>
2025-09-25 18:42:20 +0200

    Fixed code in api.ts HireSpot-Frontend

--- job_board_frontend_project/utils/api.ts
+++ job_board_frontend_project/utils/api.ts
@@ -1,7 +1,12 @@
+// utils/api.ts
 import axios from "axios";
 
+// Base URL from environment variable
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, 
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
